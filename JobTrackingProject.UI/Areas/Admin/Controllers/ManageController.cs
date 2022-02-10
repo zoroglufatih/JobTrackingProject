f62b515b4dbce58a053be53412088e5b6db1a785
yigitcankosar <40207929+yigitcankosar@users.noreply.github.com>
2022-02-10 18:45:10 +0300

    Operator register creating

--- JobTrackingProject.UI/Areas/Admin/Controllers/ManageController.cs
+++ JobTrackingProject.UI/Areas/Admin/Controllers/ManageController.cs
@@ -1,14 +1,70 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using JobTrackingProject.DTO.RegisterDTO;
+using JobTrackingProject.Entities.Concrete.Identity;
+using JobTrackingProject.UI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace JobTrackingProject.UI.Areas.Admin.Controllers
 {
-    [Authorize(Roles ="Admin")]
+    [Authorize(Roles = "Admin")]
     [Area("Admin")]
     public class ManageController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ManageController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+        public IActionResult Users()
+        {
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult OperatorRegister()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> OperatorRegisterAsync(RegisterDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null)
+            {
+                ModelState.AddModelError(nameof(model.UserName), "Bu kullanıcı adı ile kayıt yapılmıştır.");
+                return View(model);
+            }
+            user = await _userManager.FindByEmailAsync(model.Email);
+            if (user != null)
+            {
+                ModelState.AddModelError(nameof(model.Email), "Bu E-mail ile kayıt yapılmıştır.");
+                return View(model);
+            }
+            user = new ApplicationUser()
+            {
+                Name = model.Name,
+                Surname = model.Surname,
+                UserName = model.UserName,
+                Email = model.Email
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+                result = await _userManager.AddToRoleAsync(user, RoleModels.Operator);
+
+            }
             return View();
         }
     }
