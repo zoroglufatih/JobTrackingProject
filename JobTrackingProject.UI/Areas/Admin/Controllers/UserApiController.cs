bbfc9f4aad67d5339b80354db302f9103c11e887
yigitcankosar <40207929+yigitcankosar@users.noreply.github.com>
2022-02-10 16:52:58 +0300

    user api creating

--- JobTrackingProject.UI/Areas/Admin/Controllers/UserApiController.cs
+++ JobTrackingProject.UI/Areas/Admin/Controllers/UserApiController.cs
@@ -1,13 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevExtreme.AspNet.Data;
+using JobTrackingProject.Entities.Concrete.Identity;
+using JobTrackingProject.UI.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JobTrackingProject.UI.Areas.Admin.Controllers
 {
+    [Route("api/[controller]/[action]")]
+    [Authorize(Roles ="Admin")]
     public class UserApiController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public IActionResult Index()
+        public UserApiController(UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _userManager = userManager;
         }
+        [HttpGet]
+        public IActionResult Get(DataSourceLoadOptions loadOptions)
+        {
+            var data = _userManager.Users;
+            return Ok(DataSourceLoader.Load(data,loadOptions));
+        }
+        
     }
 }
