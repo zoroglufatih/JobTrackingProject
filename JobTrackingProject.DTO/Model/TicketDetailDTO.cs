146ae15055d7762d7a167251801e9fdd4243942a
yigitcankosar <40207929+yigitcankosar@users.noreply.github.com>
2022-02-23 19:45:42 +0300

    TicketDetail Page Creating

---
+++ JobTrackingProject.DTO/Model/TicketDetailDTO.cs
@@ -0,0 +1,27 @@
+﻿using JobTrackingProject.Entities.Concrete.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobTrackingProject.DTO.Model
+{
+    public class TicketDetailDTO
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Description { get; set; }
+        public int TicketId { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public List<Products> Products { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public string AddressDescription { get; set; }
+        public DateTime? TechnicianDate { get; set; }
+        public DateTime? TicketOverDate { get; set; }
+
+    }
+}
--- JobTrackingProject.UI/Areas/Admin/Controllers/TechnicianController.cs
+++ JobTrackingProject.UI/Areas/Admin/Controllers/TechnicianController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using JobTrackingProject.DataAccessLayer.Concrete.EntityFrameworkCore.Context;
+using JobTrackingProject.DTO.Model;
+using JobTrackingProject.Entities.Concrete.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +15,13 @@ namespace JobTrackingProject.UI.Areas.Admin.Controllers
     [Area("Admin")]
     public class TechnicianController : Controller
     {
+        private readonly MyContext _dbContext;
+
+        public TechnicianController(MyContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -19,9 +30,44 @@ namespace JobTrackingProject.UI.Areas.Admin.Controllers
         {
             return View();
         }
-
-        public IActionResult TicketDetail()
+        [HttpGet]
+        public IActionResult TicketDetail(int id)
         {
+            var data = _dbContext.Tickets
+                .Include(x => x.ApplicationUser)
+                .Include(x => x.Categories)
+                .ThenInclude(x => x.Products)
+                .Where(x => x.TicketId == id)
+                .FirstOrDefault();
+            if (data == null)
+            {
+                return BadRequest();
+            }
+            var model = new TicketDetailDTO()
+            {
+                AddressDescription = data.AddressDescription,
+                CategoryId = data.CategoryId,
+                CategoryName = data.Categories.CategoryName,
+                Description = data.Description,
+                Latitude = data.Latitude,
+                Longitude = data.Longitude,
+                Name = data.ApplicationUser.Name,
+                Surname = data.ApplicationUser.Surname,
+                TechnicianDate = data.TechnicianDate,
+                TicketId = data.TicketId,
+                TicketOverDate = data.TicketOverDate,
+                UserId = data.UserId,
+                Products = data.Categories.Products
+            };
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult TicketDetail(TicketDetailDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
             return View();
         }
     }
